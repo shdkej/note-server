c215bb71773ddc05bf3328df1ca43f9544aaedbd
shdkej <shdkej@github.com>
2021-06-17 10:45:30 +0900

    ci: 🎡 test

--- cmd/workflows/main_test.go
+++ cmd/workflows/main_test.go
@@ -14,8 +14,6 @@ func TestLocal(t *testing.T) {
 
 var _ = Describe("Build Workflows Test", func() {
 	Context("Get a specific workflow", func() {
-		files, _ := GetFile("/home/sh/workspace/golang/src/github.com/shdkej/note-server/snippet", ".yml")
-
 		It("get docker build image", func() {
 			d, err := GetSnippet("docker")
 			Expect(d).Should(Equal(""))
